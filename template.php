3bf954324ea1d55a9715087be9d0105e3d4cd905
Leandro Oliveira <36287525+leandromagol@users.noreply.github.com>
2019-10-11 11:52:15 -0300

    Devmind commit in 2019-10-11 11:52:14

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Gerenciador de Tarefas</title>
+    <title>TESTE de Tarefas</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
 </head>
 <body>
